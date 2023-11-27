b61761d2092c337f80210e285052c544c474bd5d
Zhixing Zhang <me@neoto.xin>
2023-11-27 03:41:43 +0000

    Add meilisearch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,13 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxt/ui',
     'nuxt-simple-sitemap',
+    'nuxt-meilisearch',
     //'@nuxtjs/web-vitals',
   ],
+  meilisearch: {
+    hostUrl:  'https://search.vkdoc.net',
+    searchApiKey: '3653930cde1ea048501e365b9cd24fc7df4a40248bb2ef185868468c3203ccf8',
+  },
   routeRules: {
     '/man/**': { isr: true },
   },
