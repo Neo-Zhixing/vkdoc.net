c6f19f8502b826380cbd3174151aba8dc87fe487
Zhixing Zhang <me@neoto.xin>
2023-11-29 08:12:53 +0000

    fix issue

--- components/content/Refpage.vue
+++ components/content/Refpage.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   name: string
   type: string
-}>()
+}>();
+
+const normalizedName = computed(() => {
+  return props.name.replaceAll('\\_', '_');
+})
 </script>
 
 <template>
-  <Card :title="name" class="my-4" :to="`/man/${name}`">
+  <Card :title="normalizedName" class="my-4" :to="`/man/${normalizedName}`">
     <ContentSlot :use="$slots.default" unwrap="p" />
   </Card>
 </template>
