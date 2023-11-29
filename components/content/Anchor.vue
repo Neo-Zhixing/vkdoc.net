5e0e87f70a2e84585336b72ed8ff21d87d714a02
Zhixing Zhang <me@neoto.xin>
2023-11-29 05:23:40 +0000

    Add anchor

---
+++ components/content/Anchor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :id="id">
+    <ContentSlot :use="$slots.default" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  id: string,
+}>();
+</script>
