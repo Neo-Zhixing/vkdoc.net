14dcca1ca7b0a52cf1edada773bf45a1ba5463b0
Zhixing Zhang <me@neoto.xin>
2023-12-02 04:05:33 +0000

    refpage improvement

--- components/content/Refpage.vue
+++ components/content/Refpage.vue
@@ -38,18 +38,20 @@ const iconName = computed(() => {
 </script>
 
 <template>
-  <div class="ring-1 shadow overflow-hidden ring-gray-200 dark:ring-gray-700 rounded-lg my-6">
-    <div class="bg-gray-100 dark:bg-gray-800 p-4 refpage-header" @click="onClick">
+  <div class="ring-1 shadow overflow-hidden ring-gray-200 dark:ring-gray-700 rounded-lg my-4">
+    <div class="bg-gray-100 dark:bg-gray-800 p-4 refpage-header not-prose" @click="onClick">
       <div class="text-sm refpage-header-title flex-col sm:flex-row">
         <b class="mr-4 text-base refpage-main-header">{{ normalizedName }}</b>
         <ContentSlot :use="$slots.default" unwrap="p" />
       </div>
-      <UIcon class="text-2xl shrink-0" :name="iconName" />
-    </div>
+        <UButton class="sm:hidden" target="_blank" color="gray" variant="solid" :to="'/man/' + normalizedName" icon="i-material-symbols-open-in-new-rounded" />
+        <UButton class="hidden sm:block" target="_blank" color="gray" variant="link" :to="'/man/' + normalizedName" label="Open" icon="i-material-symbols-open-in-new-rounded" />
+        <UIcon class="text-2xl shrink-0 !hidden sm:!block" :name="iconName" />
+      </div>
     <RefpageContent :name="normalizedName" v-if="state !== 'close'" @pending="onPendingStateChange"/>
-    <UDivider />
-    <div class="p-4 flex justify-end" v-if="state === 'ready'">
-      <UButton :to="'/man/' + normalizedName" label="Open in New Page" icon="i-heroicons-link" color="primary" />
+    <UDivider v-if="state === 'ready'" />
+    <div class="p-4 flex justify-end not-prose" v-if="state === 'ready'">
+      <UButton target="_blank" :to="'/man/' + normalizedName" label="Open in New Page" icon="i-material-symbols-open-in-new-rounded" color="primary" variant="link" />
       <UButton class="ml-2" label="Close" icon="i-heroicons-chevron-up-20-solid" color="gray" @click="state = 'close'" />
     </div>
   </div>
@@ -71,7 +73,6 @@ const iconName = computed(() => {
   overflow: hidden;
 }
 b.refpage-main-header {
-  width: 100%;
   text-overflow: ellipsis;
   overflow: hidden;
 }
