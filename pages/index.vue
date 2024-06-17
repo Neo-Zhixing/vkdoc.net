3bcb442c1ae5490d6f9a8d939564d5b86d0fbc70
Zhixing Zhang <me@neoto.xin>
2024-06-17 09:19:44 +0000

    Updating the home page

--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,22 @@ useSeoMeta({
   ogSiteName: 'VulkanHub',
   ogDescription: 'Better Vulkan Documetation starts here',
 })
+
+const colorMode = useColorMode()
+const appConfig = useAppConfig()
 </script>
 
 <template>
   <ULandingHero
     title="VulkanHub"
-    description="Better Vulkan Documentation starts here"
+    description="Better Vulkan Documentation Starts Here"
     :links="[{ label: 'Start Reading', icon: '@dust:fa6-pro-regular:book', size: 'lg', to: '/chapters/introduction' }]"
   />
   <ULandingSection title="Our Features">
     <ULandingGrid>
-      <ULandingCard class="col-span-6 row-span-2" icon="@dust:fa6-pro-regular:magnifying-glass" title="Efficient Search" description="Efficiently search the Vulkan Specification with MeiliSearch" />
+      <ULandingCard class="col-span-6 row-span-2" icon="@dust:fa6-pro-regular:magnifying-glass" title="Efficient Search" description="Efficiently search the Vulkan Specification with function names, extensions, validation codes, and more" />
       <ULandingCard class="col-span-6 row-span-4" icon="@dust:fa6-pro-regular:circle-up" title="Latest Updates" description="Stay up to date with the latest Vulkan Extensions" />
-      <ULandingCard class="col-span-6 row-span-4" icon="@dust:fa6-pro-regular:wifi-slash" title="Offline Docs" description="TODO" />
+      <ULandingCard class="col-span-6 row-span-4" :icon="colorMode.value === 'dark' ? appConfig.ui.icons.dark : appConfig.ui.icons.light" title="Light/Dark Mode" :description="(colorMode.value === 'dark' ? 'Dark' : 'Light') + ' mode is the best mode!'" />
       <ULandingCard class="col-span-6 row-span-2" icon="@dust:fa6-pro-regular:globe" title="Web-first Documentation" description="Customized conversion from AsciiDoc ensures the best reading experience on the web" />
     </ULandingGrid>
   </ULandingSection>
