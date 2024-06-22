8d302e86787b1313581ac1214ebfdf45e07afaa7
Zhixing Zhang <me@neoto.xin>
2024-06-22 02:40:21 +0000

    Add custom error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <UPageError />
+  </NuxtLayout>
+  <ClientOnly>
+    <DocSearch />
+  </ClientOnly>
+</template>
+  
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
